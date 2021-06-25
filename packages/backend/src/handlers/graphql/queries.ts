c62394efac8691f4753a8bcf321612f1a125ae83
Alec LaLonde <alec@convergencelabs.com>
2021-06-24 20:21:31 -0600

    Added guild_metadata table to hold admin-only guild information

--- packages/backend/src/handlers/graphql/queries.ts
+++ packages/backend/src/handlers/graphql/queries.ts
@@ -20,6 +20,12 @@ gql`
     }
   }
 
+  query GetPlayerByDiscordId($discordId: String) {
+    player(where: { discord_id: { _eq: $discordId } }) {
+      id
+    }
+  }
+
   query GetQuestById($quest_id: uuid!) {
     quest_by_pk(id: $quest_id) {
       id
@@ -100,4 +106,13 @@ gql`
     }
   }
   ${GuildFragment}
+
+  query GetGuildMetadata($discordId: String!) {
+    guild_metadata(where: { discord_id: { _eq: $discordId } }) {
+      guild_id
+      creator_id
+      discord_id
+      discord_metadata
+    }
+  }
 `;
